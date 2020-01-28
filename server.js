7811316cb6fb66f18fb32e4e8921051606f8ef6b
Tony Lomax <antl.lomax@gmail.com>
2020-01-28 17:12:22 +0000

    Pushing latest changes

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const axios = require("axios");
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server); // < Interesting!
+
+const fs = require("fs");
+const five = require("johnny-five");
+const board = new five.Board();
+
+const lodash = require("lodash");
+// const getApiAndEmit = async socket => {
+//   try {
+//     const res = await axios.get("https://cat-fact.herokuapp.com/facts"); // Getting the data from DarkSky
+//     console.log(
+//       "the response is",
+//       res.data.all[lodash.random(0, res.data.all.length)].text
+//     );
+//     socket.emit(
+//       "FromAPI",
+//       res.data.all[lodash.random(0, res.data.all.length)].text
+//     ); // Emitting a new message. It will be consumed by the client
+//   } catch (error) {
+//     console.error(`Error: ${error.code}`);
+//   }
+// };
+let timeCounter = 0;
+let counter = 0;
+
+board.on("ready", () => {
+  const mySensor = new five.Sensor("A0");
+
+  io.on("connection", socket => {
+    console.log("New client connected");
+
+    // setInterval(() => getApiAndEmit(socket), 3000);
+
+    mySensor.on("change", function(event) {
+      counter++;
+      console.log(counter);
+
+      if (counter % 20 === 0) {
+        const scaledData = this.scaleTo(0, 1000);
+        const timeObj = { x: timeCounter++, y: scaledData };
+        console.log(timeObj);
+        socket.emit("emg", timeObj);
+      }
+    });
+
+    socket.on("disconnect", () => console.log("Client disconnected"));
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
+import { Line } from "react-chartjs-2";
 
-function App() {
+export default function App() {
+  const [response, setResponse] = React.useState([]);
+  const [endpoint, setEndpoint] = React.useState("http://127.0.0.1:4001");
+
+  const socket = socketIOClient(endpoint);
+  socket.on("emg", data => setResponse(prevState => prevState.concat(data.y)));
+
+  const data = canvas => {
+    const ctx = canvas.getContext("2d");
+    const gradient = ctx.createLinearGradient(0, 0, 100, 0);
+
+    return {
+      backgroundColor: gradient
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      {response ? <Line data={[10, 20, 30]}></Line> : <p>Loading...</p>}
+      {response ? <p> The data is {response} </p> : <p>Loading...</p>}
     </div>
   );
 }
-
-export default App;
