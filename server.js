dea922f488d042c222073d03231f6b8876e6a14e
Jack Bridger <jacksbridger@gmail.com>
2020-01-30 15:52:58 +0000

    tidier version for review

--- server.js
+++ server.js
@@ -1,59 +1,44 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
-const axios = require("axios");
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
-app.use(index);
 const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
+const socketIo = require("socket.io");
+const io = socketIo(server, {
+  serveClient: false,
+});
+app.use(index);
+const EMG_CHANNEL = "EMG_CHANNEL"
 
-const fs = require("fs");
-const five = require("johnny-five");
-const board = new five.Board();
+// Sensor stuff - not currently using while testing
+// const five = require("johnny-five");
+// const board = new five.Board();
+// board.on("ready", () => {
+//   const mySensor = new five.Sensor("A0");
+//   mySensor.on("change", function () {
+//     currentVal = this.scaleTo(0, 1000);
+//   });
 
-const lodash = require("lodash");
-// const getApiAndEmit = async socket => {
-//   try {
-//     const res = await axios.get("https://cat-fact.herokuapp.com/facts"); // Getting the data from DarkSky
-//     console.log(
-//       "the response is",
-//       res.data.all[lodash.random(0, res.data.all.length)].text
-//     );
-//     socket.emit(
-//       "FromAPI",
-//       res.data.all[lodash.random(0, res.data.all.length)].text
-//     ); // Emitting a new message. It will be consumed by the client
-//   } catch (error) {
-//     console.error(`Error: ${error.code}`);
-//   }
-// };
-let timeCounter = 0;
-let counter = 0;
+// });
 
-board.on("ready", () => {
-  const mySensor = new five.Sensor("A0");
+let currentStrength = 0;
+let intervalID;
 
-  io.on("connection", socket => {
-    console.log("New client connected");
+io.on('connection', function (socket) {
+  console.log("user connected")
 
-    // setInterval(() => getApiAndEmit(socket), 3000);
-
-    mySensor.on("change", function (event) {
-      counter++;
-      console.log(counter);
-
-      if (counter % 100 === 0) {
-        const scaledData = this.scaleTo(0, 1000);
-        const timeObj = { x: timeCounter++, y: scaledData };
-        console.log(timeObj);
-        socket.emit("emg", timeObj);
-      }
-    });
-
-    socket.on("disconnect", () => console.log("Client disconnected"));
+  if (intervalID) {
+    clearInterval(intervalID);
+  }
+  intervalID = setInterval(() => {
+    currentStrength = Math.floor(Math.random() * 1000);
+    io.emit(EMG_CHANNEL, currentStrength)
+  }, 1000);
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
   });
 });
+
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,59 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import socketIOClient from "socket.io-client";
-import { Line } from "react-chartjs-2";
+import styled from "styled-components";
+const EMG_CHANNEL = "EMG_CHANNEL"
+
+
+const Full = styled.div`
+  border:black 5px solid;
+  width: 100px;
+  height:20px;
+  background-color:${props => props.strength >= props.threshold ? 'blue' : 'white'};
+`;
+
+
+// const socket = socketIOClient("http://127.0.0.1:4001");
+
 
 export default function App() {
-  const [response, setResponse] = React.useState([]);
-  const [endpoint, setEndpoint] = React.useState("http://127.0.0.1:4001");
-  const [refresh, setRefresh] = React.useState(false);
+  const [strength, setStrength] = React.useState(555);
 
-
-  const socket = socketIOClient(endpoint);
-  socket.on("emg", data => {
-    setResponse(prevState => {
-      if (prevState.length > 30) {
-        return prevState.slice(1).concat(data.y);
-      }
-      else {
-        return prevState.concat(data.y)
-      }
-    })
-
-  });
-
-
-  const data = {
-    datasets: [
-      {
-        label: 'My First dataset',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: response
-      }
-    ]
-  };
-
-
+  // socket.on(EMG_CHANNEL, (strengthData) => {
+  //   console.log(strengthData);
+  //   setStrength(strengthData)
+  // })
 
 
   return (
     <div style={{ textAlign: "center" }}>
-      {response ? <Line data={data} /> : <p>Loading...</p>}
+      <h1>Strength score</h1>
+      <Full strength={strength} threshold={900}></Full>
+      <Full strength={strength} threshold={800}></Full>
+      <Full strength={strength} threshold={700}></Full>
+      <Full strength={strength} threshold={600}></Full>
+      <Full strength={strength} threshold={500}></Full>
+      <Full strength={strength} threshold={400}></Full>
+      <Full strength={strength} threshold={300}></Full>
+      <Full strength={strength} threshold={200}></Full>
+      <Full strength={strength} threshold={100}></Full>
     </div>
   );
 }
