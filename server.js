1860506b9039f79c3094fcb98b22e5f78ee5a8b6
Paul Fitzgerald <paulfitz99@gmail.com>
2020-01-31 10:41:19 +0000

    update FE to display board data

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const axios = require("axios");
-const port = process.env.PORT || 4001;
 const index = require("./routes/index");
-const app = express();
-app.use(index);
-const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
-
-const fs = require("fs");
 const five = require("johnny-five");
-const board = new five.Board();
 
-const lodash = require("lodash");
-// const getApiAndEmit = async socket => {
-//   try {
-//     const res = await axios.get("https://cat-fact.herokuapp.com/facts"); // Getting the data from DarkSky
-//     console.log(
-//       "the response is",
-//       res.data.all[lodash.random(0, res.data.all.length)].text
-//     );
-//     socket.emit(
-//       "FromAPI",
-//       res.data.all[lodash.random(0, res.data.all.length)].text
-//     ); // Emitting a new message. It will be consumed by the client
-//   } catch (error) {
-//     console.error(`Error: ${error.code}`);
-//   }
-// };
+const port = process.env.PORT || 4001;
+const board = new five.Board();
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(index);
+
 let timeCounter = 0;
 let counter = 0;
 
@@ -37,21 +20,17 @@ board.on("ready", () => {
 
   io.on("connection", socket => {
     console.log("New client connected");
-
-    // setInterval(() => getApiAndEmit(socket), 3000);
-
     mySensor.on("change", function (event) {
       counter++;
-      console.log(counter);
-
       if (counter % 100 === 0) {
         const scaledData = this.scaleTo(0, 1000);
-        const timeObj = { x: timeCounter++, y: scaledData };
-        console.log(timeObj);
+        const timeObj = {
+          x: timeCounter++,
+          y: scaledData
+        };
         socket.emit("emg", timeObj);
       }
     });
-
     socket.on("disconnect", () => console.log("Client disconnected"));
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import socketIOClient from "socket.io-client";
 import { Line } from "react-chartjs-2";
 
+const ENDPOINT = "http://127.0.0.1:4001";
+const socket = socketIOClient(ENDPOINT);
+
 export default function App() {
   const [response, setResponse] = React.useState([]);
-  const [endpoint, setEndpoint] = React.useState("http://127.0.0.1:4001");
-  const [refresh, setRefresh] = React.useState(false);
 
-
-  const socket = socketIOClient(endpoint);
-  socket.on("emg", data => {
-    setResponse(prevState => {
-      if (prevState.length > 30) {
-        return prevState.slice(1).concat(data.y);
-      }
-      else {
-        return prevState.concat(data.y)
-      }
-    })
-
-  });
-
+  React.useEffect(() => {
+    socket.on("emg", data => {
+      // do stuff with  your data below
+      setResponse(response => [...response, data.y]);
+    });
+    return () => {
+      console.log("Component unmounted");
+      socket.close();
+    };
+  }, []);
 
   const data = {
     datasets: [
       {
-        label: 'My First dataset',
+        label: "My First dataset",
         fill: false,
         lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
         borderDash: [],
         borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
         pointBorderWidth: 1,
         pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
@@ -47,9 +44,6 @@ export default function App() {
       }
     ]
   };
-
-
-
 
   return (
     <div style={{ textAlign: "center" }}>
