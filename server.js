852289dbd9141b3225f79178f8e5761e75da1ef7
Paul Fitzgerald <paulfitz99@gmail.com>
2020-01-31 09:25:07 +0000

    display data with cats

--- server.js
+++ server.js
@@ -1,59 +1,43 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
 const axios = require("axios");
-const port = process.env.PORT || 4001;
+const socketIo = require("socket.io");
 const index = require("./routes/index");
-const app = express();
-app.use(index);
-const server = http.createServer(app);
-const io = socketIo(server); // < Interesting!
-
-const fs = require("fs");
-const five = require("johnny-five");
-const board = new five.Board();
-
 const lodash = require("lodash");
-// const getApiAndEmit = async socket => {
-//   try {
-//     const res = await axios.get("https://cat-fact.herokuapp.com/facts"); // Getting the data from DarkSky
-//     console.log(
-//       "the response is",
-//       res.data.all[lodash.random(0, res.data.all.length)].text
-//     );
-//     socket.emit(
-//       "FromAPI",
-//       res.data.all[lodash.random(0, res.data.all.length)].text
-//     ); // Emitting a new message. It will be consumed by the client
-//   } catch (error) {
-//     console.error(`Error: ${error.code}`);
-//   }
-// };
-let timeCounter = 0;
-let counter = 0;
+const app = express();
 
-board.on("ready", () => {
-  const mySensor = new five.Sensor("A0");
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = process.env.PORT || 4001;
 
-  io.on("connection", socket => {
-    console.log("New client connected");
+app.use(index);
 
-    // setInterval(() => getApiAndEmit(socket), 3000);
+const getApiAndEmit = async socket => {
+  try {
+    const res = await axios.get("https://cat-fact.herokuapp.com/facts"); // Getting the data from DarkSky
+    console.log(
+      "the response is",
+      res.data.all[lodash.random(0, res.data.all.length)].text
+    );
+    socket.emit(
+      "FromAPI",
+      res.data.all[lodash.random(0, res.data.all.length)].text
+    );
+  } catch (error) {
+    console.error(`Error: ${error.code}`);
+  }
+};
 
-    mySensor.on("change", function (event) {
-      counter++;
-      console.log(counter);
 
-      if (counter % 100 === 0) {
-        const scaledData = this.scaleTo(0, 1000);
-        const timeObj = { x: timeCounter++, y: scaledData };
-        console.log(timeObj);
-        socket.emit("emg", timeObj);
-      }
-    });
-
-    socket.on("disconnect", () => console.log("Client disconnected"));
+io.on("connection", socket => {
+  console.log("New client connected");
+  setInterval(() => getApiAndEmit(socket), 3000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -1,59 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import socketIOClient from "socket.io-client";
-import { Line } from "react-chartjs-2";
+
+const ENDPOINT = "http://localhost:4001";
+const socket = socketIOClient(ENDPOINT);
 
 export default function App() {
   const [response, setResponse] = React.useState([]);
-  const [endpoint, setEndpoint] = React.useState("http://127.0.0.1:4001");
-  const [refresh, setRefresh] = React.useState(false);
 
+  React.useEffect(() => {
+    socket.on("FromAPI", data => {
+      setResponse(response => [...response, data]);
+    });
 
-  const socket = socketIOClient(endpoint);
-  socket.on("emg", data => {
-    setResponse(prevState => {
-      if (prevState.length > 30) {
-        return prevState.slice(1).concat(data.y);
-      }
-      else {
-        return prevState.concat(data.y)
-      }
-    })
-
-  });
-
-
-  const data = {
-    datasets: [
-      {
-        label: 'My First dataset',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: response
-      }
-    ]
-  };
-
-
-
+    return () => {
+      console.log("Component unmounted");
+      socket.close();
+    };
+  }, []);
 
   return (
-    <div style={{ textAlign: "center" }}>
-      {response ? <Line data={data} /> : <p>Loading...</p>}
+    <div>
+      {response.length > 0 ? (
+        response.map((item, index) => <p key={index}>{item}</p>)
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
